ddb1cc8e54941222cc742cd20b2b2c72d3241a65
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-13 07:25:33 +0300

    Initial Commit

---
+++ SwiftUIid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIid
+//
+//  Created by Ramill Ibragimov on 13.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIid/SwiftUIidApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIidApp.swift
+//  SwiftUIid
+//
+//  Created by Ramill Ibragimov on 13.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
